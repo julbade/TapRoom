d79ca558496f499b4d52f8fe69cc7f49f0441646
Akjol Jaenbai and Julius Bade <akjol@>
2018-10-24 16:39:58 -0700

    add more css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,54 +10,54 @@ export class AppComponent {
   title = 'Tap Room';
   pint = 124;
   taps: Tap[] = [
-  new Tap('KillerVodka', 'Proper 12', 15, 40, 'Strawberry'),
-  new Tap('DrinkAndFight', 'Chans', 25, 80, 'Alcohol'),
-  new Tap('AjFucker', 'AJ', 10, 90, 'Alcohol')
-];
+    new Tap('KillerVodka', 'Proper 12', 15, 40, 'Strawberry'),
+    new Tap('DrinkAndFight', 'Chans', 25, 80, 'Alcohol'),
+    new Tap('AjFucker', 'AJ', 10, 90, 'Alcohol')
+  ];
+  
+  selectedTap = null;
+  newTap = null;
 
-selectedTap = null;
-newTap = null;
 
 
+  editTap(clickedTap) {
+    this.selectedTap = clickedTap;
+  }
+  finishEdit() {
+    this.selectedTap = null;
 
-editTap(clickedTap) {
-  this.selectedTap = clickedTap;
-}
-finishEdit() {
-  this.selectedTap = null;
-
-}
-addTap() {
-  let addTap = new Tap();
-  this.newTap = addTap;
-  this.taps.push(this.newTap);
-}
-sale(currentTap){
-  currentTap.price *= .5;
+  }
+  addTap() {
+    let addTap = new Tap();
+    this.newTap = addTap;
+    this.taps.push(this.newTap);
+  }
+  sale(currentTap){
+    currentTap.price *= .5;
   }
 
-delete(currentTap) {
-  let index = this.taps.indexOf(currentTap);
-  this.taps.splice(index, 1);
-}
+  delete(currentTap) {
+    let index = this.taps.indexOf(currentTap);
+    this.taps.splice(index, 1);
+  }
 
-doneAdding(){
-  this.newTap = 0;
-}
-fillTap(currentTap){
-  currentTap.pint = 124;
-}
+  doneAdding(){
+    this.newTap = 0;
+  }
+  fillTap(currentTap){
+    currentTap.pint = 124;
+  }
 
-sellTap(currentTap){
-  currentTap.pint--;
-}
-sellGrowler(currentTap){
-  currentTap.pint-=2;
-}
-sellBigGrowler(currentTap){
-  currentTap.pint-=5;
-}
-pintColor(currentTap){
+  sellTap(currentTap){
+    currentTap.pint--;
+  }
+  sellGrowler(currentTap){
+    currentTap.pint-=2;
+  }
+  sellBigGrowler(currentTap){
+    currentTap.pint-=5;
+  }
+  pintColor(currentTap){
     if (currentTap.pint <= 10){
       return "bg-danger";
     } else if (currentTap.pint <= 70 && currentTap.pint > 10) {
@@ -67,12 +67,12 @@ pintColor(currentTap){
     }
   }
   priceColor(currentTap){
-      if (currentTap.price >= 20){
-        return "bg-danger";
-      } else if (currentTap.price < 20 && currentTap.price > 10) {
-        return  "bg-warning";
-      } else {
-        return "bg-info";
-      }
+    if (currentTap.price >= 20){
+      return "bg-danger";
+    } else if (currentTap.price < 20 && currentTap.price > 10) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
     }
+  }
 }
